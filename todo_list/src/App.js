f448f6f3619991487f12e26394c1fb42eda0bacb
Josmar Diaz <jdm.begreat28@gmail.com>
2023-08-24 19:48:28 -0400

    Created Form + Input for To Do List + Functionality to Capture Input

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-
+import React from 'react';
+import ToDoForm from "./components/ToDoForm/ToDoForm";
 
 function App() {
   return (
     <div className="App">
-      TO-DO LIST
+      TO DO LIST
+      <ToDoForm />
     </div>
   );
 }
--- todo_list/src/components/ToDoForm/ToDoForm.jsx
+++ todo_list/src/components/ToDoForm/ToDoForm.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDoForm = () => {
+
+    const [task, setTask] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newTask = {
+            task: task
+        };
+        console.log(newTask);
+    }
+
     return ( 
-        <div>
-            ToDoForm
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+            <input type='text' placeholder='Create Task' onChange={(event) => setTask(event.target.value)} />
+            <button type='submit'>Add</button>
+            </div>
+        </form>
      );
 }
  
